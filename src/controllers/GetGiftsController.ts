8fbaf86addfd5ebbeeae288ffc60ce576523303e
Joana <joanajanefdez@gmail.com>
2024-12-01 15:29:07 +0100

    refactor getGiftsController getGifts

--- src/controllers/GetGiftsController.ts
+++ src/controllers/GetGiftsController.ts
@@ -1,23 +1,20 @@
 import {GiftRepository} from "../GiftRepository";
 import {Request, Response} from 'express';
-import {Gift} from "../Gift";
 
 export class GetGiftsController{
     giftRepository: GiftRepository;
-    giftsList: Gift[];
 
     constructor() {
         this.giftRepository = GiftRepository.getInstance();
-        this.giftsList = this.giftRepository.getGifts();
     };
 
     GetAll(req: Request, res: Response){
-        res.send(JSON.stringify(this.giftsList));
+        res.send(JSON.stringify(this.giftRepository.getGifts()));
     };
 
     GetToWho(req: Request, res: Response){
         const { toWho } = req.params;
-        res.send(JSON.stringify(this.giftsList[toWho]));
+        res.send(JSON.stringify(this.giftRepository.getGifts()[toWho]));
     };
 }
 
